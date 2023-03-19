6ad7365b4bc2cb5d9685db03ad266d5449c64859
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-03-19 20:54:11 -0300

    chore: create a

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+matrix = np.loadtxt('simple_input.txt', usecols=range(6))
+print (matrix)
+
+def matrix_transfrom():
+    return 0
+
+
+def choose_way():
+    return 0
+
+
+def run():
+	return 0
+
+
+# while True:
+#     run()
+
+print(matrix_transfrom())
