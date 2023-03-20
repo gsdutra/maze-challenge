37057e974dab25e8b2e1fabe775ee3474259aa94
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-03-20 17:08:50 -0300

    chore: implement pathfinder function

--- main.py
+++ main.py
@@ -7,11 +7,45 @@ print (matrix)
 matrix_len_x = matrix.shape[1]
 matrix_len_y = matrix.shape[0]
 
-def choose_way():
+brk = [0]
+
+def choose_way(pos, history, matrix, depth):
+
+	if depth > 15: brk [2]
+
+	print (pos, history)
+
+	if (matrix[pos[0], pos[1]] == 4):
+		return history
+	
+	if (pos[0] + 1 != matrix_len_y):
+		if (matrix_transfrom(matrix)[pos[0] + 1, pos[1]] != 1):
+			history.append('D')
+			ans = choose_way([pos[0] + 1, pos[1]], history, matrix, depth + 1)
+			if ans != 0:
+				return ans
+
+	if (pos[1] + 1 != matrix_len_x):
+		if (matrix_transfrom(matrix)[pos[0], pos[1] + 1] != 1):
+			history.append('L')
+			ans = choose_way([pos[0], pos[1] + 1], history, matrix, depth + 1)
+			if ans != 0:
+				return ans
+
+	if (pos[0] - 1 != -1):
+		if (matrix_transfrom(matrix)[pos[0] - 1, pos[1]] != 1):
+			history.append('U')
+			ans = choose_way([pos[0] - 1, pos[1]], history, matrix, depth + 1)
+			if ans != 0:
+				return ans
+
+	if (pos[1] - 1 != -1):
+		if (matrix_transfrom(matrix)[pos[0], pos[1] - 1] != 1):
+			history.append('R')
+			ans = choose_way([pos[0], pos[1] - 1], history, matrix, depth + 1)
+			if ans != 0:
+				return ans
+
 	return 0
 
-
-def run():
-	return 0
-
-print(matrix_transfrom(matrix))
+print(choose_way([0,0], [], matrix, 0))
